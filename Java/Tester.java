d40fda03846a155cd2c18a46f1f31a71a86dece2
Jayatwo <jayatwo.7501@gmail.com>
2025-01-29 16:03:44 +0530

    Created Tester.java
    Created this tester class for checking github actions of code scanning using code ql

---
+++ Java/Tester.java
@@ -0,0 +1,11 @@
+package com.demo;
+import java.util.Scanner;
+public class Tester{
+  public static void main(String [] args){
+    System.out.println("Hey hii this is sample text for preparing github");
+    System.out.println("What is ur name?");
+    Scanner s = new Scanner(System.in);
+    String name = s.next();
+    s.close();
+  }
+}
