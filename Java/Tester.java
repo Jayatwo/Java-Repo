85a6cb2838dc9ab9fd0f9f11beff58f17a2cf956
Jayatwo <jayatwo.7501@gmail.com>
2025-01-29 16:20:33 +0530

    Update Tester.java

--- Java/Tester.java
+++ Java/Tester.java
@@ -1,7 +1,9 @@
 
 public class Tester{
   public static void main(String [] args){
-    String password = "12345";
+    String usernaem = "admin";//hardcoded username
+    String password = "12345";//hardcoded password
+    System.out.println("The username is: " + username);
     System.out.println("The pasword is: "+ password);
   }
 }
