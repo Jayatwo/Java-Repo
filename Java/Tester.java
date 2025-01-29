b779fa1b8524af08a7c323131fbc86dce81d25b2
Jayatwo <jayatwo.7501@gmail.com>
2025-01-29 16:13:54 +0530

    Update Tester.java
    Just modified code

--- Java/Tester.java
+++ Java/Tester.java
@@ -1,11 +1,7 @@
-package com.demo;
-import java.util.Scanner;
+
 public class Tester{
   public static void main(String [] args){
-    System.out.println("Hey hii this is sample text for preparing github");
-    System.out.println("What is ur name?");
-    Scanner s = new Scanner(System.in);
-    String name = s.next();
-    s.close();
+    String password = "12345";
+    System.out.println("The pasword is: "+ password);
   }
 }
